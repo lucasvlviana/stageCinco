d316eb632f67d004f72574a7b357d3a13271db76
lucasvlviana <70477354+lucasvlviana@users.noreply.github.com>
2023-04-15 13:16:45 -0300

    editando o main.js

--- jogoAdivinhacao/main.js
+++ jogoAdivinhacao/main.js
@@ -1,7 +1,13 @@
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 1
+
 function handleClick(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector('#inputNumber')
 
-    console.log(inputNumber.value)
+    xAttempts++
+
+    console.log(xAttempts)
 }
